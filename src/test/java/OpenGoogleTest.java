26fc3fae0a3fd2db9825f578f14f35e56cddece8
sandaliDil <dsandali290@gmail.com>
2024-07-06 10:16:08 +0530

    Update last commit

--- src/test/java/OpenGoogleTest.java
+++ src/test/java/OpenGoogleTest.java
@@ -3,13 +3,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class OpenGoogleTest {
 
     @Test
     public  void googleTest(){
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("123");
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--headless=new");
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.google.com/");
         driver.findElement(By.id("APjFqb")).sendKeys("colombo" + Keys.ENTER);
     }
